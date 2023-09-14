157da42cdfcc35d0bc01c9b9af061bfb815fcf4b
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-09-14 13:17:21 +0200

    Yikers

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+function showProfilebar() {
+    const sidebar = document.querySelector('.profilebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideProfilebar() {
+    const sidebar = document.querySelector('.profilebar')
+    sidebar.style.display = 'none'
+}
